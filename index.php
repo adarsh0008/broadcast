30a25846cd2d149622995c53fb4ff4d8c1359def
ajayprakashshukla1 <ajayprakashshukla1@gmail.com>
2020-04-18 14:16:28 +0530

    Removed HTML file

--- index.php
+++ index.php
@@ -170,7 +170,7 @@ audio
     var name = $("#input_1").val();
     var dname = $("#input_2").val();
     var med=$('#input_3 :selected').val();
-    var url= "https://virtualsquad.herokuapp.com/prev.php?name="+name+"&dname="+dname+"&med="+med;
+    var url= "https://virtualsquad.herokuapp.com/prev.php?name="+name+"%26dname="+dname+"%26med="+med;
     alert(url);
     ("#wshare").attr("href","whatsapp://send?text="+url);
    // alert("whatsapp://send?text=Dear "+dname+"! "+name+" Dedicated a song to you.please enjoy your song here :"+url);
@@ -184,7 +184,7 @@ audio
       <label for="input_4">
       Send to whatsapp
         </label>
-   <a id="wshare" href="whatsapp://send?text=https://virtualsquad.herokuapp.com/prev.php?name=adarsh&dname=arti&med=b" data-action="share/whatsapp/share"><img src="1.jpg"></a>
+   <a id="wshare" href="whatsapp://send?text=ttt" data-action="share/whatsapp/share"><img src="1.jpg"></a>
 </div>
 
 
