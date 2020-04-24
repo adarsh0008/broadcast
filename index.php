dfc970af1277fda8ed7ea3fdf04d1766762831c1
ajayprakashshukla1 <ajayprakashshukla1@gmail.com>
2020-04-24 15:30:13 +0530

    firebase login

--- index.php
+++ index.php
@@ -39,14 +39,35 @@
   var password="password";
   
   //Create User with Email and Password
-  firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
+  firebase.auth().createUserWithEmailAndPassword(email, password).then(function() {
+  // User deleted.
+  console.log('User Created!');
+}).catch(function(error) {
     // Handle Errors here.
     var errorCode = error.code;
     var errorMessage = error.message;
     console.log(errorCode);
     console.log(errorMessage);
   });
-  
+// function to check user is signed in or not
+  firebase.auth().onAuthStateChanged(function(user) {
+  if (user) {
+    // User is signed in.
+    var displayName = user.displayName;
+    var email = user.email;
+    var emailVerified = user.emailVerified;
+    var photoURL = user.photoURL;
+    var isAnonymous = user.isAnonymous;
+    var uid = user.uid;
+    var providerData = user.providerData;
+
+    console.log("user id for logged in user is :"+uid);
+    // ...
+  } else {
+    // User is signed out.
+    // ...
+  }
+});
 </script>
 
 
